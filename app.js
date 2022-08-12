016b72e7172935b7c82bc5984dc9c908ec110c07
Magdalena Łątka <magude6@gmail.com>
2022-08-12 21:30:45 +0200

    Refactor code in app.js

--- app.js
+++ app.js
@@ -11,55 +11,55 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-app.get("/", function (_req, res) {
-  res.sendFile(__dirname + "/signup.html");
-})
+app.get("/", (_req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+  })
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_TOKEN,
   server: "us8",
 });
 
-app.post("/", function(req, res) {
-  const listId = process.env.MAILCHIMP_LIST_ID;
+app.post("/", (req, res) => {
+    const listId = process.env.MAILCHIMP_LIST_ID;
 
-  const name = req.body.userName;
-  const surname = req.body.userSurname;
-  const email = req.body.userEmail;
+    const name = req.body.userName;
+    const surname = req.body.userSurname;
+    const email = req.body.userEmail;
 
-  const subscribingUser = {
-    firstName: name,
-    lastName: surname,
-    email: email
-  };
+    const subscribingUser = {
+      firstName: name,
+      lastName: surname,
+      email: email
+    };
 
-  async function run() {
-    try {
-      const response = await mailchimp.lists.addListMember(listId, {
-        email_address: subscribingUser.email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: subscribingUser.firstName,
-          LNAME: subscribingUser.lastName
-        }
-      });
+    async function run() {
+      try {
+        const response = await mailchimp.lists.addListMember(listId, {
+          email_address: subscribingUser.email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: subscribingUser.firstName,
+            LNAME: subscribingUser.lastName
+          }
+        });
 
-      console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
-      res.sendFile(__dirname + "/success.html");
+        console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
+        res.sendFile(__dirname + "/success.html");
 
-    } catch (e) {
-      console.log(e.status);
-      console.log("Something went wrong...");
-      res.sendFile(__dirname + "/failure.html");
+      } catch (e) {
+        console.log(e.status);
+        console.log("Something went wrong...");
+        res.sendFile(__dirname + "/failure.html");
+      }
     }
-  }
 
-  run();
-});
+    run();
+  });
 
-app.post("/failure", function(_req, res) {
-  res.redirect("/");
-})
+app.post("/failure", (_req, res) => {
+    res.redirect("/");
+  })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000...");
